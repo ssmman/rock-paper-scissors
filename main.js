1bb5c31504e69930092e73268ceff5341334e7d0
ssmman <48299327+ssmman@users.noreply.github.com>
2020-02-05 20:37:20 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,82 +1,52 @@
+const no_tie_results = [2, 0, 1];
+
+
 function calculate ()
 {
-	var form = document.forms["main_form"];
-	var you_threw = parseInt (form.elements[0].value, 10);
-	var opp_threw = parseInt (form.elements[1].value, 10);
-
-	switch (opp_threw)
-	{
-		case 0:
-			switch (you_threw)
-			{
-				case 0:
-					print_results (99);
-					break;
-				case 1:
-					print_results (0);
-					break;
-				case 2:
-					print_results (1);
-					break;
-			}
-			break;
-		case 1:
-			switch (you_threw)
-			{
-				case 0:
-					print_results (2);
-					break;
-				case 1:
-					print_results (99);
-					break;
-				case 2:
-					print_results (1);
-					break;
-			}
-			break;
-		case 2:
-			switch (you_threw)
-			{
-				case 0:
-					print_results (2);
-					break;
-				case 1:
-					print_results (0);
-					break;
-				case 2:
-					print_results (99);
-					break;
-			}
-			break;	
-	}
+    var form = document.forms["main_form"];
+    var you_threw = form.you_played.value;
+    print_results (no_tie_results[you_threw]);
 }
 
 
 function number_to_item (input)
 {
-	switch (input)
-	{
-		case 0:
-			return "rock";
-			break;
-		case 1:
-			return "paper";
-			break;
-		case 2:
-			return "scissors";
-			break;
-	}
+    switch (input)
+    {
+        case 0:
+            return "rock";
+            break;
+        case 1:
+            return "paper";
+            break;
+        case 2:
+            return "scissors";
+            break;
+    }
 }
 
 
 function print_results (input)
 {
-	var output;
-	
-	if (input === 99)
-		output = "it was a tie! just play whatever you want; it makes no difference"
-	else
-		output = number_to_item (input) + " has the greatest of chance of winning the next round.";
-	
-	document.getElementById ("result").innerHTML = output;
+    var output;
+
+    if (document.getElementById("checkbox").checked)
+    {
+        switch (input)
+        {
+            case 0:
+                output = "paper or scissors has the greatest chance of winning the next round.";
+                break;
+            case 1:
+                output = "rock or scissors has the greatest chance of winning the next round.";
+                break;
+            case 2:
+                output = "rock or paper has the greatest chance of winning the next round.";
+                break;
+        }
+    }        
+    else
+        output = number_to_item (input) + " has the greatest of chance of winning the next round.";
+
+    document.getElementById ("result").innerHTML = output;
 }
